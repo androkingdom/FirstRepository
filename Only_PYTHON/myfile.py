0b075e234844fb7e5a790f7837ca3c109ee77a2b
andro <androkingdom1@gmail.com>
2023-01-10 16:11:45 +0530

    add myfile.py with patter printting program

--- Only_PYTHON/myfile.py
+++ Only_PYTHON/myfile.py
@@ -1,6 +1,18 @@
-a = {1,2,3,4,5,6}
-b = {1,74,7,3,23,2,32}
-c = {38,48,347,3}
-print(a)
-print(b)
-print(c)
+# a = {1,2,3,4,5,6}
+# b = {1,74,7,3,23,2,32}
+# c = {38,48,347,3}
+# print(a)
+# print(b)
+# print(c)
+
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+print("I have done it !!")
+
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
