0b9a090a92333d28a1c55d34104be06ed44968f7
andro <androkingdom1@gmail.com>
2023-01-16 22:24:29 +0530

    new program

--- Only_PYTHON/myfile.py
+++ Only_PYTHON/myfile.py
@@ -5,14 +5,37 @@
 # print(b)
 # print(c)
 
-for i in range(6,0,-1):
-    for j in range(i):
-        print("*",end=" ")
-    print()
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
 
-print("I have done it !!")
+# print("I have done it !!")
 
-for i in range(1,6):
-    for j in range(i):
-        print("*",end=" ")
-    print()
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+import os
+choice = input("Do wo allow to make file : ")
+if choice == "yes" or choice == "Yes": 
+    try:
+        os.mkdir("Detail")
+        with open("Detail/Det.txt","w") as f:
+            f.read()
+    except:
+        print("Created a new file")
+    print("ENTER")
+    print("1. TO ENTER DATA")
+    print("2. TO REVIRT DATA")
+    choice2 = int(input("==> "))
+    if choice2 == 1:
+        data = input("Enter data : ")
+        with open("Detail/Det.txt","wt") as f:
+            f.write(f"{data}")
+    elif choice2 == 2:
+        with open("Detail/Det.txt","r") as f:
+            print(f.read())
+else :
+    print("Please allow permission")
