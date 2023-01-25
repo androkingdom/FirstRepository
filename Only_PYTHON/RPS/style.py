009f0b1eb814bcac1a4884301fb3a4c3697abead
andro <androkingdom1@gmail.com>
2023-01-25 22:49:50 +0530

    more styles

--- Only_PYTHON/RPS/style.py
+++ Only_PYTHON/RPS/style.py
@@ -1,3 +1,6 @@
+# ------------
+# | =>       |
+# ------------
 def style(x):
     l = len(x)
     for i in range(l+7):
@@ -8,3 +11,31 @@ def style(x):
     for i in range(l+7):
         print("-",end="")
     print()
+
+#  = = = =
+# ||     ||
+#  = = = =
+def style2(x):
+    l = len(x)
+    for i in range(l-1):
+        print(" =",end="")
+    print()
+    print(f"||{x}||")
+    for i in range(l-1):
+        print(" =",end="")
+
+#  _____
+# |     |
+#  -----
+def style3(x):
+    l = len(x)
+    print(" ",end="")
+    for i in range(l):
+        print("_",end="")
+    print()
+    print(f"|{x}|")
+    print(" ",end="")
+    for i in range(l):
+        print("-",end="")
+
+style3("hello")
