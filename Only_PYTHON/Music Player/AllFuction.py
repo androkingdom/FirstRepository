02843547d94420c2ba8b81aa716b93af50c50a37
andro <androkingdom1@gmail.com>
2023-03-16 19:28:40 +0530

    Music Player

--- Only_PYTHON/Music Player/AllFuction.py
+++ Only_PYTHON/Music Player/AllFuction.py
@@ -12,11 +12,18 @@ def player(location):
     mixer.music.set_volume(0.7)
     mixer.music.play()
     print("Playing....")
+    print("Enter 'e' to stop\nEnetr 'p' to pause\nEnter 'r' to resume")
     while True:
-        stopper = input("Enter 'e' to stop : ")
+        stopper = input("Enter : ")
         if stopper == 'e':
             mixer.music.stop()
             break 
+        elif stopper == 'p':
+            mixer.music.pause()
+            print("Music Paused")
+        elif stopper == 'r':
+            mixer.music.play()
+            print("Music Resumed")
         else:
             print("Some error ocurred")
     
