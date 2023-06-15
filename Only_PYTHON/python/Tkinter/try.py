848b8dcebffd22b3aedd351494f384553473d396
androkingdom <androkingdom1@gmail.com>
2023-06-15 20:46:56 +0530

    updating

--- Only_PYTHON/python/Tkinter/try.py
+++ Only_PYTHON/python/Tkinter/try.py
@@ -1,13 +1,18 @@
-import tkinter as tk
-from tkinter import ttk
-import time as tt
+'''
+    1
+   2 2
+ 3    3
+4      4
+'''
 
-root = tk.Tk()
-label = ttk.Label(root , text='Hello World')
-
-label.pack()
-
-tt.sleep(1)
-print('Closing')
-root.destroy
-root.mainloop()
+txt = '    '
+txt2 = ' '
+for i in range(1,5):
+    print(f'{txt}{i}',end='')
+    txt = txt[:-1]
+    if i > 1  :
+        for j in range(5-i):
+            print(f'{txt2}{i}',end=' ')
+            txt2 += '  '
+            break
+    print()
