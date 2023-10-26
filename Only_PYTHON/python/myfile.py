b864297d3010bd7f8529bbe0907b1288df088725
androkingdom <androkingdom1@gmail.com>
2023-10-26 21:16:36 +0530

    update

--- Only_PYTHON/python/myfile.py
+++ Only_PYTHON/python/myfile.py
@@ -61,9 +61,9 @@
 *####
 '''
 
-# for i in range(4,0,-1):
-#     for j in range(i):
-#         print('*',end='')
-#     for k in range(5-i):
-#             print('#',end='')
-#     print()
+for i in range(4,0,-1):
+    for j in range(i):
+        print('*',end='')
+    for k in range(5-i):
+            print('#',end='')
+    print()
