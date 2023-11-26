640fdb4b4b4ee2b60a698b17d5c4e5c5f54bfe7d
androkingdom <androkingdom1@gmail.com>
2023-11-26 11:24:11 +0530

    RockPaperScissor

--- Python/RPS/game.py
+++ Python/RPS/game.py
@@ -2,11 +2,19 @@ import style
 import random
 
 def result(usersel , compu):
+
+    dic = {1:{2:"loss",3:"won",1:"draw"},
+    2:{3:"loss",1:"won",2:"draw"},
+    3:{1:"loss",2:"won",3:"draw"}}
+
+    meaning = {1:"Rock",2:"Paper",3:"Scissor"}
+
     if usersel not in dic:
         print("Your : Enter Invaild Input")
 
     elif compu not in dic:
         print("Comp. : Enter Invaild Input")
+        print(compu)
 
     else:
         print()
@@ -16,15 +24,69 @@ def result(usersel , compu):
         print(f"Comp. : {meaning[compu]}")
         style.style(f"Result : {result}")
 
-# rock --> paper
-# paper --> scissor
-# scissor --> rock
+# 1=> rock --> 2=> paper
+# 2=> paper --> 3=> scissor
+# 3=> scissor --> 1=> rock
 
-def hard(usersel):
 
-    pass
+def HardLevel(usersel):
+    CompuResult = 0 
+    user = usersel
+    Score = 0
+    OutputDict = {}
+    HardGameDict = {1:2,2:3,3:1}
+    EasyGameDict = {1:3,2:1,3:2}
+    HardChance = random.randint(1,100)
+    if HardChance <= 90 and HardChance >= 1:
+        CompuResult = HardGameDict[user]
+    elif HardChance < 95  and HardChance > 90 :
+        CompuResult = user
+    else :
+        CompuResult = EasyGameDict[user]
+        Score += 1
+    OutputDict["CompuResult"] = CompuResult
+    OutputDict["Score"] = Score
+    return OutputDict
+
+def EasyLevel(usersel):
+    CompuResult = 0 
+    user = usersel
+    Score = 0
+    OutputDict = {}
+    EasyChance = random.randint(1,100)
+    HardGameDict = {1:2,2:3,3:1}
+    EasyGameDict = {1:3,2:1,3:2}
+    if EasyChance <= 90 and EasyChance >= 1:
+        CompuResult = EasyGameDict[user]
+        Score += 1
+    elif EasyChance < 95  and EasyChance > 90 :
+        CompuResult = user
+    else :
+        CompuResult = HardGameDict[user]
+    OutputDict["CompuResult"] = CompuResult
+    OutputDict["Score"] = Score
+    return OutputDict
+
+def NormalLevel(usersel):
+    CompuResult = 0 
+    user = usersel
+    Score = 0
+    OutputDict = {}
+    NormalChance = random.randint(1,100)
+    HardGameDict = {1:2,2:3,3:1}
+    EasyGameDict = {1:3,2:1,3:2}
+    if NormalChance > 50:
+        CompuResult = HardGameDict[user]
+    elif NormalChance < 50:
+        CompuResult = EasyGameDict[user]
+        Score += 1
+    OutputDict["CompuResult"] = CompuResult
+    OutputDict["Score"] = Score
+    return OutputDict
+
+
 print("-------------------------")   #Seprator
-# tml
+
 # Level Selection
 
 LevelDict = {1:"Hard",2:"Easy",3:"Normal"}
@@ -36,46 +98,49 @@ print()
 level_selection = int(input("Enter Level Number : "))
 print()
 
-chances = random.choice([i for i in range(1,11)])
+NumberOfRound = int(input("Enter Number Of Round : "))
 
-dic = {1:{2:"loss",3:"won",1:"draw"},
-2:{3:"loss",1:"won",2:"draw"},
-3:{1:"loss",2:"won",3:"draw"}}
-
-meaning = {1:"Rock",2:"Paper",3:"Scissor"}
-print(chances)
-
-print('''1 --> Rock
-2 --> Paper
-3 --> Scissors''')
+print('1 --> Rock \n2 --> Paper \n3 --> Scissors')
 print()
 
-match level_selection :
-    case 1:
-        print("You have selected Hard Level")
-        print("===================")
-        if chances <= 8 and chances > 5 :
-            selection = int(input("Enter Choice Number: "))
-            comp = random.randrange(1,4)
-            result(selection,comp)
+NetScore = 0
 
-    case 2:
-        print("You have selected Easy Level")
-        print("===================")
-        if chances == 1 :
-            selection = int(input("Enter Choice Number: "))
-            comp = random.randrange(1,4)
-            result(selection,comp)
+try:
+    for RoundNumber in range(NumberOfRound):
+        print("=================")
+        print(f"Round - {RoundNumber + 1}")
+        print("=================")
+        match level_selection :
+            case 1:
+                print("============================")
+                print("You have selected Hard Level")
+                print("============================")
+                selection = int(input("Enter Choice Number: "))
+                ResultingDict = HardLevel(selection)
+                comp = ResultingDict["CompuResult"]
+                NetScore += ResultingDict["Score"]
+                result(selection,comp)
 
-    case 3:
-        print("You have selected Normal Level")
-        print("===================")
-        selection = int(input("Enter Choice Number: "))
-        comp = random.randrange(1,4)
-        result(selection,comp)
+            case 2:
+                print("============================")
+                print("You have selected Easy Level")
+                print("============================")
+                selection = int(input("Enter Choice Number: "))
+                ResultingDict = EasyLevel(selection)
+                comp = ResultingDict["CompuResult"]
+                NetScore += ResultingDict["Score"]
+                result(selection,comp)
 
+            case 3:
+                print("==============================")
+                print("You have selected Normal Level")
+                print("==============================")
+                selection = int(input("Enter Choice Number: "))
+                ResultingDict = NormalLevel(selection)
+                comp = ResultingDict["CompuResult"]
+                NetScore += ResultingDict["Score"]
+                result(selection,comp)
 
-
-# 1 --> Rock
-# 2 --> Paper
-# 3 --> Scissors
+    print(f"Net Score => {NetScore}")
+except Exception as e:
+    print(e)
