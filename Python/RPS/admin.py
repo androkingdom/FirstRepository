787b1616c6e5f077983bc1c0751a034cdc27e32d
androkingdom <androkingdom1@gmail.com>
2023-11-28 17:27:42 +0530

    RockPaperScissor

--- Python/RPS/admin.py
+++ Python/RPS/admin.py
@@ -1,5 +1,33 @@
 import function as func
 
+print("==================")
+print("Select Choice :-")
+print("1 : Score History")
+print("2 : Max Played History")
+print("==================")
 
-dfs = func.retrive_db_as_df("recoreds")
-print(dfs)
+choice = int(input("Enter Choice Number : "))
+
+if choice == 1:
+    # print("==================")
+    # print("1 : Graph View")
+    # print("2 : Database View")
+    # print("==================")
+    # choice_inside = int(input("Enter Choice Number : "))
+    # match choice_inside:
+        # case 1 :
+    func.graph_Score()
+        # case 2 : 
+    print(func.df_of_score)
+
+elif choice == 2:
+    # print("==================")
+    # print("1 : Graph View")
+    # print("2 : Database View")
+    # print("==================")
+    # choice_inside = int(input("Enter Choice Number : "))
+    # match choice_inside:
+        # case 1 :
+    func.graph_Max_Played()
+        # case 2 : 
+    print(func.df_of_max_played)
--- Python/RPS/function.py
+++ Python/RPS/function.py
@@ -1,10 +1,18 @@
+# Library Used 
 import random
 import style
 import pandas as pd
 import mysql.connector as sqLtor
 import datetime
+from numpy import random
+from sqlalchemy import create_engine
+from matplotlib import  pyplot as plt
 
+# Global Variables Used
+df_of_score = 0
+df_of_max_played = 0
 
+# Give The Result Of Game
 def result(usersel , compu):
 
     dic = {1:{2:"loss",3:"won",1:"draw"},
@@ -32,7 +40,7 @@ def result(usersel , compu):
 # 2=> paper --> 3=> scissor
 # 3=> scissor --> 1=> rock
 
-
+# Set Game Level To Hard
 def HardLevel(usersel):
     CompuResult = 0 
     user = usersel
@@ -41,7 +49,7 @@ def HardLevel(usersel):
     HardGameDict = {1:2,2:3,3:1}
     EasyGameDict = {1:3,2:1,3:2}
     HardChance = random.randint(1,100)
-    if HardChance <= 90 and HardChance >= 1:
+    if HardChance <= 80 and HardChance >= 1:
         CompuResult = HardGameDict[user]
     elif HardChance < 95  and HardChance > 90 :
         CompuResult = user
@@ -52,6 +60,7 @@ def HardLevel(usersel):
     OutputDict["Score"] = Score
     return OutputDict
 
+# Set Game Level To Easy
 def EasyLevel(usersel):
     CompuResult = 0 
     user = usersel
@@ -60,7 +69,7 @@ def EasyLevel(usersel):
     EasyChance = random.randint(1,100)
     HardGameDict = {1:2,2:3,3:1}
     EasyGameDict = {1:3,2:1,3:2}
-    if EasyChance <= 90 and EasyChance >= 1:
+    if EasyChance <= 80 and EasyChance >= 1:
         CompuResult = EasyGameDict[user]
         Score += 1
     elif EasyChance < 95  and EasyChance > 90 :
@@ -71,6 +80,7 @@ def EasyLevel(usersel):
     OutputDict["Score"] = Score
     return OutputDict
 
+# Set Game Level To Normal
 def NormalLevel(usersel):
     CompuResult = 0 
     user = usersel
@@ -88,6 +98,7 @@ def NormalLevel(usersel):
     OutputDict["Score"] = Score
     return OutputDict
 
+# Create Database Named Recoreds
 def create_database(DatabaseName):
     try:
         myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306")
@@ -96,7 +107,8 @@ def create_database(DatabaseName):
         myconnection.close()
     except Exception as e:
         print(e)
-    
+
+# Create Tabele Named History To Store Data
 def create_table(TableName,dbs):
     myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306" , database=dbs)
     cursor = myconnection.cursor()
@@ -108,27 +120,63 @@ def create_table(TableName,dbs):
     cursor.execute(query)
     myconnection.close()
 
+# To Return TimeStamp When Called
 def timestamp_now():
         now = datetime.datetime.now()
         formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
         return formatted_now
 
+# Used To Enter Date To Specific Table
 def insert_data(TableName,dbs,Value_):
-    myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306" , database=dbs)
+    try:
+        myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306" , database=dbs)
+        cursor = myconnection.cursor()
+        query = f"""INSERT INTO {TableName} (LEVEL_SELECTED,SCORES,TIMESTAMP) VALUES(%s
+        ,%s,%s)"""
+        values = tuple(Value_)
+        cursor.execute(query,values)
+        myconnection.commit()
+        myconnection.close()
+    except:
+        print("You Enter Wrong Input")
+
+# Used To Plot Graph On Score History
+def graph_Score():
+    global df_of_score
+    query2 = '''SELECT LEVEL_SELECTED , SUM(scores) as total_played from history group by level_selected'''
+    myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306" , database="recoreds")
     cursor = myconnection.cursor()
-    query = f"""INSERT INTO {TableName} (LEVEL_SELECTED,SCORES,TIMESTAMP) VALUES(%s
-    ,%s,%s)"""
-    values = tuple(Value_)
-    cursor.execute(query,values)
-    myconnection.commit()
+    cursor.execute(query2)
+    result = cursor.fetchall()
+    df1 = pd.DataFrame(result, columns=['Level Selected', 'Score'])
+    df_of_score = df1
+    plt.bar(df1['Level Selected'], df1['Score'])
+    plt.xlabel('Level')
+    plt.ylabel('Score')
+    plt.title('Score Graph')
+    plt.yticks([])
+    plt.show()
     myconnection.close()
 
-def retrive_db_as_df(dbs):
-    myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306" , database=dbs)
-    query = "SELECT LEVEL_SELECTED , SUM(scores) as Total_Score from history GROUP by level_selected;"
-    df = pd.read_sql(query,myconnection)
-    return df
+# Used To Plot Graph On Max Played History
+def graph_Max_Played():
+    global df_of_max_played
+    query1 = '''SELECT LEVEL_SELECTED , COUNT(level_selected) as total_played from history group by level_selected'''
+    myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306" , database="recoreds")
+    cursor = myconnection.cursor()
+    cursor.execute(query1)
+    result = cursor.fetchall()
+    df1 = pd.DataFrame(result, columns=['Level Selected', 'Total Played'])
+    df_of_max_played = df1
+    plt.bar(df1['Level Selected'], df1['Total Played'])
+    plt.xlabel('Level')
+    plt.ylabel('Total Played')
+    plt.title('Time Spent Graph')
+    plt.yticks([])
+    plt.show()
+    myconnection.close()
 
+# Used Delete All Recoreds From Table
 def delete_history(tbname , dbs):
     myconnection = sqLtor.connect( host = 'localhost' , user = 'root', password = '1234',port="3306" , database=dbs)
     cursor = myconnection.cursor()
