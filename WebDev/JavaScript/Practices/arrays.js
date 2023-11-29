a74ffbe988521a3df33a91dd65c0f07810787c7c
androkingdom <androkingdom1@gmail.com>
2023-11-29 16:46:31 +0530

    JavaScript

---
+++ WebDev/JavaScript/Practices/arrays.js
@@ -0,0 +1,8 @@
+let arry = [-4,-3,-2,-1,0,1,2,3,4]
+console.log(arry)
+// console.log(arry.filter(e=>{if (e < 0){return true} return false}))
+// console.log(arry.filter(e=>{if (e < 0){return false} return true}))
+// console.log(arry.map(e=>{return e**2}))
+// console.log(arry.length)
+// console.log(arry.join(''))
+// console.log(arry.toString())
---
+++ WebDev/JavaScript/Practices/if_elise_if_else.js
@@ -0,0 +1,20 @@
+a = 10
+
+if (a < 8){
+    console.log("ooo")
+}
+
+if (a == 10){
+    console.log("again oooo")
+}
+
+else if (a == 10){
+    console.log("again oooo")
+}
+
+else {
+    console.log("LOL")
+}
+
+let c = a < 10 ? (a-2):(a)
+console.log(c)
