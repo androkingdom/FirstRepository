0522b9c027ddf54b39a17f8c62c95051c957c08e
androkingdom <androkingdom1@gmail.com>
2023-12-04 12:35:55 +0530

    JavaScript DOMs

---
+++ WebDev/JavaScript/Practices/DOM/index.js
@@ -0,0 +1,11 @@
+// Chage backgroundColor
+document.body.style.backgroundColor = "red" ;
+// Chage Title
+document.title = "Document Obejct Model In JavaScript" ;
+//Change Element Content
+// 1> By ClassName
+var elementByClassName = document.getElementsByClassName("box1").innerHTML = "Box1 - Changed";
+var elementByClassName = document.getElementsByClassName("box2").innerHTML = "Box1 - Changed" ;
+var elementByClassName = document.getElementsByClassName("box3").innerHTML = "Box1 - Changed" ;
+// Similar with ID
+// Syntax ==> document.getElementsById(<Id Name>).innerHTML = "<Content to be changed>";
