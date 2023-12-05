e4275614ad5fe1a7972705cd003a0160c9b56026
androkingdom <androkingdom1@gmail.com>
2023-12-05 18:45:41 +0530

    Hangman - Word Guessing Game

--- Python/Hangman/main.py
+++ Python/Hangman/main.py
@@ -3,33 +3,26 @@ import random
 
 
 # Variable Used
-words = '''ant baboon badger bat bear beaver camel cat clam cobra cougar
-       coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
-       lion lizard llama mole monkey moose mouse mule newt otter owl panda
-       parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep
-       skunk sloth snake spider stork swan tiger toad trout turkey turtle
-       weasel whale wolf wombat zebra'''.split()
+words = '''Lion Tiger Leopard Cheetah Elephant Giraffe Rhinoceros Gorilla Chimpanzee Wolf Bear Panda 
+            Kangaroo Koala Zebra Hippopotamus Crocodile Alligator Cheetah Bison zebra'''.split()
 
 words_list = list(words)
-getword = random.choice(words_list)
+getword = random.choice(words_list).lower()
 list_of_cover = []
 list_of_getword = list(getword)
-uncovering_word = "".join(list_of_cover)
+uncovering_word = 0
+word_used = []
 chance_to_uncover = 0
 
 # Functio To Be Used
-def covers():
-    global getword
+def create_covers():
+    global getword, uncovering_word
     for i in range(len(getword)):
         list_of_cover.append("_")
+    uncovering_word = "".join(list_of_cover)
 
-def change_covers(x = ""):
+def change_covers(x):
     global list_of_cover, list_of_getword, uncovering_word
-    # if x not in list_of_getword:
-    #     print("Not a Letter")
-    # elif x in list_of_getword:
-    #     print("Letter Founded")
-
     for i in list_of_getword:
         if i == x:
             indx = list_of_getword.index(i)
@@ -37,33 +30,35 @@ def change_covers(x = ""):
             list_of_getword[indx] = ""
     uncovering_word = "".join(list_of_cover)
 
-def alert(x):
-    global list_of_cover, list_of_getword, uncovering_word
-    if x not in list_of_getword:
-        print("==============")
-        print("Not a Letter")
-        print("==============")
-    elif x in list_of_getword:
-        print("==============")
-        print("Letter Founded")
-        print("==============")
+# working
+create_covers()
 
-# Working :-
-covers()
-change_covers()
-print("=========================")
-print("Welcome To HangMan Game")
-print("=========================")
-while uncovering_word != getword:
-    print(uncovering_word , end=" ")
-    letter = input("Enter a Letter : ")
-    if len(letter) != 1:
-        print("!! You Are Disqualified To Enter More Than One Letter !!")
-        break
+print("==============================================")
+print("Welcome To HangMan Game - Living Thing Version")
+print("==============================================")
+for i in range(7):
     chance_to_uncover += 1
-    change_covers(letter)
-    alert(letter)
+    print(uncovering_word , end=" ")
+    letter = input("Guess The Letter : ").lower()
+    print()
+    if letter in list_of_getword:
+        print("You Found A Letter")
+    if letter not in list_of_getword:
+        print("Wrong Letter")
+    print()
+    if letter not in word_used:
+        word_used.append(letter)
+        change_covers(letter)
+        if uncovering_word == getword:
+            print(f"{uncovering_word} : You Find The Word")
+            break
+        elif len(letter) != 1:
+            print("!! You Are Disqualified To Enter More Than One Letter !!")
+            break
+    else:
+        print("Letter Already Used")
 
 if uncovering_word == getword:
-    print("Your find the word")
     print(f"You Won The Game In {chance_to_uncover} Chance")
+elif uncovering_word != getword:
+    print(f"You Loss The Game Correct Word Is {getword}")
