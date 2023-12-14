5ee4d09f462e0c280ea613959a41caa936434430
androkingdom <androkingdom1@gmail.com>
2023-12-14 16:29:36 +0530

    commit all

---
+++ WebDev/JavaScript/PracticeSet/Ch-8/script.js
@@ -0,0 +1,8 @@
+// Question - 1
+let btn_list = Array.from(document.querySelectorAll(".btns"))
+btn_list[0].addEventListener("click",()=>alert("Opps!!"))
+btn_list[1].addEventListener("click",()=>alert("Yay!!"))
+btn_list[2].addEventListener("click",()=>alert("Ahhh!!"))
+btn_list[3].addEventListener("click",()=>alert("Don't Do!!"))
+
+// Question - 2
